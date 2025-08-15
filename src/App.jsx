3b41c7009b5449547335286466b037108a2f57bf
Mubin Shikalgar <mubinshikalgars@gmail.com>
2025-08-15 21:59:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ProfileHead from "./components/ProfileHead";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div className="justify-items-center">
+      <ProfileHead />
+      <Skills />
+      <Experience />
+    </div>
+  );
+}
+
+export default App;
